375e087dd4429f57eb65e8c3f8b76d9285a0e68f
Alzando <alzandotetuko@gmail.com>
2024-11-26 11:23:29 +0700

    Update login_screen.dart

--- lib/Login/login_screen.dart
+++ lib/Login/login_screen.dart
@@ -1,46 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 import 'package:monitoringobat/components/responsive.dart';
 import '../../components/background.dart';
 import 'components/login_form.dart';
 import 'components/login_screen_top_image.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoadingWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          child: Lottie.asset(
+            'assets/lottie/main_loading.json',
+            fit: BoxFit.contain,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool isLoading = false;
+
+  Widget LoadingWidget() {
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: Lottie.asset(
+          'assets/lottie/loading.json',
+          width: 200,
+          height: 200,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Background(
-        child: Container(
-            height: MediaQuery.of(context).size.height,
-            child: SingleChildScrollView(
-              child: Responsive(
-                mobile: const MobileLoginScreen(),
-                desktop: Row(
-                  children: [
-                    const Expanded(
-                      child: LoginScreenTopImage(),
-                    ),
-                    Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          SizedBox(
-                            width: 450,
-                            child: LoginForm(),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+      child: isLoading ? LoadingWidget() : Container(
+        height: MediaQuery.of(context).size.height,
+        child: SingleChildScrollView(
+          child: Responsive(
+            mobile: const MobileLoginScreen(),
+            desktop: Row(
+              children: [
+                const Expanded(
+                  child: LoginScreenTopImage(),
                 ),
-              ),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      SizedBox(
+                        width: 450,
+                        child: LoginForm(),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            decoration: BoxDecoration(
-                image: DecorationImage(
-              image: AssetImage("assets/images/bgmonevminumobatbaru.png"),
-              fit: BoxFit.cover,
-            ))));
+          ),
+        ),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/bgmonevminumobatbaru.png"),
+            fit: BoxFit.cover,
+          ))));
   }
 }
 
