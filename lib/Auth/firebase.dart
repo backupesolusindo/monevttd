a8d25e8d5f2b6cf60c93ecfd6cc24a3bed07d7ae
Alzando <alzandotetuko@gmail.com>
2024-11-26 11:21:33 +0700

    Update firebase.dart

--- lib/Auth/firebase.dart
+++ lib/Auth/firebase.dart
@@ -4,8 +4,23 @@ class FirebaseApi {
   final _firebaseMessaging = FirebaseMessaging.instance;
 
   Future<void> initNotification() async {
-    await _firebaseMessaging.requestPermission();
-    final fcmToken = await _firebaseMessaging.getToken();
-    print('Token Firebase: $fcmToken');
+    try {
+      print('\n=================== CHECKING FIREBASE ===================');
+      // Get FCM token
+      final fcmToken = await _firebaseMessaging.getToken();
+      print('FCM TOKEN: $fcmToken');
+      print('=====================================================\n');
+
+      // Request permission
+      final settings = await _firebaseMessaging.requestPermission(
+        alert: true,
+        badge: true,
+        sound: true,
+      );
+      print('Permission status: ${settings.authorizationStatus}');
+
+    } catch (e) {
+      print('Error getting FCM token: $e');
+    }
   }
 }
